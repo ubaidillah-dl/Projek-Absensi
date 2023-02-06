a3e43083050281f1d9fe761195281f032509382f
ubaidillah-dl <ubayd2947@gmail.com>
2023-02-06 20:14:45 +0700

    update 13

--- absensi/index.php
+++ absensi/index.php
@@ -1,8 +1,3 @@
-<?php
-    if (session_status() === PHP_SESSION_NONE) {
-        session_start();
-    }
-    
-    if (empty($_SESSION['akun'])) {
-        include __DIR__ . "./masuk.php";
-    }
+<?php 
+    header("location: ../masuk.php");
+?>
